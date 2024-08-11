d93cc91edd1bb9180ec2003a6e0b595ea033f0a7
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-08-10 23:00:48 -0300

    Upload do projeto

---
+++ controller/cliente/controller-cliente.js
@@ -0,0 +1,28 @@
+module.exports = () => {
+    const controller = {};
+    const clientes = [];    
+
+    controller.listar = (req, res) => {        
+        res.status(200).json(clientes);
+    };
+
+    controller.buscarPorId = (req, res) => { 
+        console.log('Buscar por id');
+        console.log(req.param("id"));    
+        res.status(200).json(clientes.filter(cliente => cliente.id == req.param("id"))[0]);
+    };
+
+    controller.salvar = (req, res) => {    
+        const cliente = req.body;
+        
+        clientes.push(cliente);
+
+        cliente.id = clientes.length;
+        res.status(201).json(cliente);
+    };
+
+    controller.excluir = (req, res) => {                
+    };
+    
+    return controller;
+}
---
+++ controller/produto/controller-produto.js
@@ -0,0 +1,28 @@
+module.exports = () => {
+    const controller = {};
+    const produtos = [];    
+
+    controller.listar = (req, res) => {        
+        res.status(200).json(produtos);
+    };
+
+    controller.buscarPorId = (req, res) => { 
+        console.log('Buscar por id');
+        console.log(req.param("id"));    
+        res.status(200).json(produtos.filter(produto => produto.id == req.param("id"))[0]);
+    };
+
+    controller.salvar = (req, res) => {    
+        const produto = req.body;
+        
+        produtos.push(produto);
+
+        produto.id = produtos.length;
+        res.status(201).json(produto);
+    };
+
+    controller.excluir = (req, res) => {                
+    };
+    
+    return controller;
+}
